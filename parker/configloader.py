4a1f2b5aaae4470f2f471f4b8c727d3004081b34
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-06-18 11:53:26 +0100

    Add load_site_config to configloader module.

--- parker/configloader.py
+++ parker/configloader.py
@@ -10,6 +10,7 @@ CONFIG_PATH = os.path.join(
     'etc',
     'parker'
 )
+CONFIG_SITES_PATH = 'sites'
 
 
 def _load_config_json(file_path):
@@ -22,6 +23,17 @@ def load_config(name):
     return _load_config_json(
         os.path.join(
             CONFIG_PATH,
+            name + CONFIG_EXT
+        )
+    )
+
+
+def load_site_config(name):
+    """Load and return site configuration as a dict."""
+    return _load_config_json(
+        os.path.join(
+            CONFIG_PATH,
+            CONFIG_SITES_PATH,
             name + CONFIG_EXT
         )
     )
--- test/test_configloader.py
+++ test/test_configloader.py
@@ -2,11 +2,34 @@
 """Test the configuration loader."""
 
 import pytest
-from parker.configloader import load_config
+from parker.configloader import load_config, load_site_config
 
 TEST_CONFIG_NAME = 'redis'
 TEST_CONFIG_KEY = 'host'
 TEST_CONFIG_VALUE = 'localhost'
+TEST_SITE_NAME = 'staples'
+EXPECTED_SITE_CONFIG = {
+    "id": "staples",
+    "uri_base": "http://www.staples.co.uk/",
+    "uri_start_crawl": "http://www.staples.co.uk/",
+    "specific_data": {
+        "title": {
+            "selector": "h1",
+            "regex_filter": "(\\w+)"
+        }
+    },
+    "key_value_data": {
+        "key_selector": "#divSpecifications dd .l",
+        "value_selector": "#divSpecifications dd .r"
+    },
+    "crumbs": {
+        "selector": "#skuBreadCrumbs span[itemprop=title]"
+    },
+    "media": {
+        "selector": ".s7Thumbs img",
+        "attribute": "data-zoomimage"
+    }
+}
 
 
 def test_load_config_loads_json_into_a_dict():
@@ -14,3 +37,12 @@ def test_load_config_loads_json_into_a_dict():
     config = load_config(TEST_CONFIG_NAME)
 
     assert config[TEST_CONFIG_KEY] == TEST_CONFIG_VALUE
+
+
+def test_load_site_config_loads_json_into_a_dict():
+    """Test 'staples' test content is loaded correctly."""
+    site_config = load_site_config(TEST_SITE_NAME)
+
+    print site_config
+
+    assert site_config == EXPECTED_SITE_CONFIG
