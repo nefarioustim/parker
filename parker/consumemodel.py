e049be59e2be6d6b9b45c1bf8ee51e858e5047a1
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-06-22 23:29:45 +0100

    Add consumemodel.load_from_config.

--- parker/consumemodel.py
+++ parker/consumemodel.py
@@ -34,3 +34,48 @@ class ConsumeModel(object):
     def __init__(self, consumepage):
         """Constructor."""
         self.consumepage = consumepage
+        self.uri = consumepage.uri
+        self.data_dict = None
+        self.key_value_dict = None
+        self.crumb_list = None
+        self.media_list = None
+
+    def load_from_config(self, config):
+        """Load model from passed configuration."""
+        data_config = config.get("specific_data", False)
+        key_value_config = config.get("key_value_data", False)
+        crumb_config = config.get("crumbs", False)
+        media_config = config.get("media", False)
+
+        # Populate specific data dictionary
+        self.data_dict = self.consumepage.get_data_dict_from_config(
+            data_config
+        ) if data_config else None
+
+        # Populate key value dictionary
+        self.key_value_dict = self.consumepage.get_key_value_dict_by_selectors(
+            key_value_config["key_selector"],
+            key_value_config["value_selector"]
+        ) if (
+            key_value_config
+            and key_value_config.get("key_selector", False)
+            and key_value_config.get("value_selector", False)
+        ) else None
+
+        # Populate crumb list
+        self.crumb_list = self.consumepage.get_crumb_list_by_selector(
+            crumb_config["selector"]
+        ) if (
+            crumb_config
+            and crumb_config.get("selector", False)
+        ) else None
+
+        # Populate media list
+        self.media_list = self.consumepage.get_media_list_by_selector(
+            media_config["selector"],
+            media_config["attribute"],
+        ) if (
+            media_config
+            and media_config.get("selector", False)
+            and media_config.get("attribute", False)
+        ) else None
--- test/test_consumemodel.py
+++ test/test_consumemodel.py
@@ -3,11 +3,39 @@
 
 import pytest
 from parker import consumemodel
+from parker.configloader import load_site_config
 from test_client import client_fixture
 from test_page import page_fixture
 from test_consumepage import consumepage_fixture
 
 TEST_URI = "http://www.staples.co.uk/full-strip-stapler/cbs/412852.html"
+TEST_CONFIG_NAME = "staples"
+TEST_CONFIG = load_site_config(TEST_CONFIG_NAME)
+EXPECTED_KV_DICT = {
+    u'Staple Compatibility :': u'26/06/2014',
+    u'Brands :': u'Staples',
+    u'Stapling Capacity (sheets) :': u'20 sheets',
+    u'Colour :': u'Black',
+    u'Quantity :': u'1'
+}
+EXPECTED_CRUMB_LIST = [
+    u'Pens | Tape | Desk Supplies',
+    u'Desktop Stationery | Clips | Accessories',
+    u'Staplers'
+]
+EXPECTED_MEDIA_LIST = [
+    '//www.staples.co.uk/content/images/product/uk_412852_1_xnl.jpg'
+]
+EXPECTED_DATA_DICT = {
+    'title': 'Full Strip Stapler',
+    'sku': 'WW-412852'
+}
+
+
+@pytest.fixture(scope="function")
+def consumemodel_fixture(consumepage_fixture):
+    """Test fixture to ensure correct mocking for parsedpage."""
+    return consumemodel.get_instance(consumepage_fixture)
 
 
 def test_get_instance_creates_consumemodel_object(consumepage_fixture):
@@ -21,3 +49,14 @@ def test_get_instance_raises_typeerror_unexpected_parameter_type():
     """Test consumemodel.get_instance throws TypeError on unexpected param."""
     with pytest.raises(TypeError):
         test_consumemodel = consumemodel.get_instance(None)
+
+
+def test_load_from_config_populates_model(consumemodel_fixture):
+    """Test consumemodel.load_from_config populates the model members."""
+    test_consumemodel = consumemodel_fixture
+    test_consumemodel.load_from_config(TEST_CONFIG)
+
+    assert test_consumemodel.data_dict == EXPECTED_DATA_DICT
+    assert test_consumemodel.key_value_dict == EXPECTED_KV_DICT
+    assert test_consumemodel.crumb_list == EXPECTED_CRUMB_LIST
+    assert test_consumemodel.media_list == EXPECTED_MEDIA_LIST
