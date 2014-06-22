b365a3b5be84beb891f5cafd240607d49b3eb7ef
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-06-22 13:13:47 +0100

    Translate the get_instance upgrades to consumemodel.

--- parker/consumemodel.py
+++ parker/consumemodel.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 """Model object for consumed pages in Parker."""
 
+import consumepage
+
 _instances = dict()
 
 
-def get_instance(uri):
+def get_instance(page_to_consume):
     """Return an instance of ConsumeModel."""
+    if isinstance(page_to_consume, str):
+        uri = page_to_consume
+        page_to_consume = consumepage.get_instance(uri)
+    elif isinstance(page_to_consume, consumepage.ConsumePage):
+        uri = page_to_consume.uri
+    else:
+        raise TypeError(
+            "get_instance() expects a parker.ConsumePage or str derivative."
+        )
+
     try:
         instance = _instances[uri]
     except KeyError:
-        instance = ConsumeModel()
+        instance = ConsumeModel(page_to_consume)
         _instances[uri] = instance
 
     return instance
@@ -19,5 +31,6 @@ class ConsumeModel(object):
 
     """Model representing a consumed page."""
 
-    def __init__(self):
+    def __init__(self, consumepage):
         """Constructor."""
+        self.consumepage = consumepage
--- test/test_consumemodel.py
+++ test/test_consumemodel.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 """Test the ConsumeModel object."""
 
+import pytest
 from parker import consumemodel
+from test_client import client_fixture
+from test_page import page_fixture
+from test_consumepage import consumepage_fixture
 
 TEST_URI = "http://www.staples.co.uk/full-strip-stapler/cbs/412852.html"
 
 
-def test_get_instance_creates_consumemodel_object():
+def test_get_instance_creates_consumemodel_object(consumepage_fixture):
     """Test consumemodel.get_instance creates a ConsumeModel object."""
-    test_consumemodel = consumemodel.get_instance(TEST_URI)
+    test_consumemodel = consumemodel.get_instance(consumepage_fixture)
 
     assert isinstance(test_consumemodel, consumemodel.ConsumeModel)
+
+
+def test_get_instance_raises_typeerror_unexpected_parameter_type():
+    """Test consumemodel.get_instance throws TypeError on unexpected param."""
+    with pytest.raises(TypeError):
+        test_consumemodel = consumemodel.get_instance(None)
