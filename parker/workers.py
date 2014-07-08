829a9a0e45d4e040c229e83b93a2cd2245e3863a
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-07-08 13:17:13 +0100

    Add consumer worker.

--- parker/workers.py
+++ parker/workers.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
 """Worker methods for Parker."""
+
+import os.path
+import consumemodel
+from configloader import load_site_config
+from fileops import DATA_DIR
+
+
+def consumer(site, uri):
+    """Consume URI using site config."""
+    model = consumemodel.get_instance(uri)
+    model.load_from_config(
+        load_site_config(site)
+    )
+    model.save_to_file(
+        os.path.join(
+            DATA_DIR,
+            site + '.data'
+        )
+    )
--- test/test_workers.py
+++ test/test_workers.py
@@ -1,2 +1,33 @@
 # -*- coding: utf-8 -*-
 """Test the worker methods."""
+
+import os.path
+from parker import fileops
+from parker.workers import consumer
+from test_client import client_fixture
+
+TEST_URI = "http://www.staples.co.uk/full-strip-stapler/cbs/412852.html"
+TEST_SITE = "staples"
+EXPECTED_FILE = os.path.join(
+    fileops.DATA_DIR,
+    TEST_SITE + '.data'
+)
+EXPECTED_DATA_DICT = {
+    'title': 'Full Strip Stapler',
+    'sku': 'WW-412852'
+}
+
+
+def test_consumer_writes_data_to_file(client_fixture):
+    """Test workers.consumer writes correct data to file system."""
+    consumer(TEST_SITE, TEST_URI)
+
+    assert os.path.exists(EXPECTED_FILE)
+    assert os.path.isfile(EXPECTED_FILE)
+
+    line = fileops.get_line_from_file(EXPECTED_FILE).next()
+
+    for key, value in EXPECTED_DATA_DICT.iteritems():
+        assert value in line
+
+    os.remove(EXPECTED_FILE)
