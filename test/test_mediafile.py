6f6968f145a791f98a5c5e30440d1acceba6a61b
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-07-10 17:51:59 +0100

    Add a mediafile fixture, use it on test_fetch_to_file_downloads_to_file, and remove the skip if offline.

--- test/test_mediafile.py
+++ test/test_mediafile.py
@@ -4,11 +4,23 @@
 import os
 import pytest
 from parker import mediafile, client
+from test_client import client_fixture
 import utils
 
 TEST_URI = "http://www.staples.co.uk/content/images/product/uk_412852_1_xnl.jpg"
 TEST_FILE = "/tmp/somewhere"
 EXPECTED_FILENAME = "/tmp/somewhere.jpg"
+
+
+@pytest.fixture(scope="function")
+def mediafile_fixture(client_fixture):
+    """Test fixture to ensure correct mocking for mediafile."""
+    test_mediafile = mediafile.get_instance(
+        uri=TEST_URI
+    )
+    test_mediafile.client = client_fixture
+
+    return test_mediafile
 
 
 def test_get_instance_creates_mediafile_object():
@@ -23,10 +35,9 @@ def test_get_instance_creates_mediafile_object():
     assert test_mediafile.__repr__() == expected_repr
 
 
-@pytest.mark.skipif(not utils.is_online(), reason="Currently offline.")
-def test_fetch_to_file_downloads_to_file():
+def test_fetch_to_file_downloads_to_file(mediafile_fixture):
     """Test mediafile.fetch_to_file downloads to file."""
-    test_mediafile = mediafile.get_instance(TEST_URI)
+    test_mediafile = mediafile_fixture
     test_mediafile.fetch_to_file(TEST_FILE)
 
     assert test_mediafile.filename == EXPECTED_FILENAME
