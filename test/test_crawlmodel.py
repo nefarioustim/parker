c78e67d09df501ebc5ad41221b6577488c877b8e
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-07-18 19:35:28 +0100

    Add another test to check parameter on get_instance.

--- test/test_crawlmodel.py
+++ test/test_crawlmodel.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test the CrawlModel object."""
 
+import pytest
 from parker import crawlmodel
 from test_client import client_fixture_crawl
 from test_page import page_fixture_crawl
@@ -12,3 +13,9 @@ def test_get_instance_creates_crawlmodel_object(crawlpage_fixture):
     test_crawlmodel = crawlmodel.get_instance(crawlpage_fixture)
 
     assert isinstance(test_crawlmodel, crawlmodel.CrawlModel)
+
+
+def test_get_instance_raises_typeerror_unexpected_parameter_type():
+    """Test crawlmodel.get_instance throws TypeError on unexpected param."""
+    with pytest.raises(TypeError):
+        test_crawlmodel = crawlmodel.get_instance(None)
