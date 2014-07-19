7c3cc3edfdedda729a09a21af9b49f2561218338
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-07-19 09:05:23 +0100

    Update Staples configuration to include consume_selector.

--- test/test_configloader.py
+++ test/test_configloader.py
@@ -12,6 +12,7 @@ EXPECTED_SITE_CONFIG = {
     "id": "staples",
     "uri_base": "http://www.staples.co.uk/",
     "uri_start_crawl": "http://www.staples.co.uk/",
+    "consume_selector": "#PageInner .skuPage",
     "unique_field": "sku",
     "specific_data": {
         "title": {
