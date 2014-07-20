7ba784a2535465e110aba9c7e401dec4281d2279
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-07-20 12:55:20 +0100

    Update setup to use the updated README and CHANGES docs as the long description.

--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
-"""
-Parker
------
+# -*- coding: utf-8 -*-
+"""Parker.
 
-Parker is a Python-based web spider for collecting specific data across a set
-of configured sites.
-
+A web spider for collecting specific data
+across a set of configured sites.
 """
 
+import os
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+
+
+def read(*names):
+    """Read files for description."""
+    values = dict()
+    for name in names:
+        filename = name + '.rst'
+        if os.path.isfile(filename):
+            fd = open(filename)
+            value = fd.read()
+            fd.close()
+        else:
+            value = ''
+        values[name] = value
+    return values
+
+
+long_description = """
+%(README)s
+
+News
+====
+
+%(CHANGES)s
+
+""" % read('README', 'CHANGES')
 
 version_file = open('VERSION')
 version = version_file.read().strip()
@@ -25,7 +50,7 @@ setup(
     author_email='tim@nefariousdesigns.co.uk',
     description='A web spider for collecting specific data across a set of '
                 'configured sites',
-    long_description=__doc__,
+    long_description=long_description,
     install_requires=reqs,
     packages=['parker'],
     scripts=[
